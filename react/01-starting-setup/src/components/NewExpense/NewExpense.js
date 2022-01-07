7821c3fe28c8239f015a9a16b9da861354bdba49
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-07 14:56:27 -0500

    new prop for ExpenseForm to pass data to parent component

--- react/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ react/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,12 +1,23 @@
-import './NewExpense.css'
-import ExpenseForm from './ExpenseForm';
-
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+// component
 const NewExpense = () => {
-    return (
-        <div className='new-expense'>
-            <ExpenseForm />
-        </div>
-    );
+  // function
+  const SaveDataHandler = (enteredData) => {
+    // object
+    const expenseData = {
+      ...enteredData,
+      // enhance obj with an id
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveData={SaveDataHandler} />
+    </div>
+  );
 };
 
 export default NewExpense;
