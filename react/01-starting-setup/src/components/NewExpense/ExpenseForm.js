190b6c4bea2df144945cd221be6e8f867f094675
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-07 14:57:28 -0500

    cleaned up and passed expenseData up to parent

--- react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,64 +1,42 @@
 import "./ExpenseForm.css";
 import { useState } from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+  // state variables
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredNumber, setEnteredNumber] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-  // alternative way to do this is with one single state
-  // where we pass an object
-//   const [userInput, setUserInput] = useState({
-//       enteredTitle: '',
-//       enteredNumber: '',
-//       enteredDate: ''
-//   });
-
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-
-    // setUserInput({
-    //     /// spread operator to bring in current state of userInput
-    //     ...userInput,
-    //     // override the title change
-    //     enteredTitle: event.target.value,
-    // });
   };
 
   const numberChangeHandler = (event) => {
     setEnteredNumber(event.target.value);
-
-    // setUserInput({
-    //     ...userInput,
-    //     enteredNumber: event.target.value,
-    // });
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-
-    // setUserInput({
-    //     ...userInput,
-    //     enteredDate: event.target.value,
-    // });
   };
 
   const submitHandler = (event) => {
     // prevent the default of this request being sent
     event.preventDefault();
 
+    // build new object
     const expenseData = {
-        title: enteredTitle,
-        number: enteredNumber,
-        date: new Date(enteredDate)
-    }
+      title: enteredTitle,
+      number: enteredNumber,
+      date: new Date(enteredDate),
+    };
 
-    console.log(expenseData);
+    // pass new object to be used in NewExpense
+    props.onSaveData(expenseData);
 
     // after submission, reset the state of the form back to blank
-    setEnteredTitle('');
-    setEnteredDate('');
-    setEnteredNumber('');
+    setEnteredTitle("");
+    setEnteredDate("");
+    setEnteredNumber("");
   };
 
   return (
@@ -66,15 +44,31 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" value={enteredNumber} min="0.01" step="0.01" onChange={numberChangeHandler}/>
+          <input
+            type="number"
+            value={enteredNumber}
+            min="0.01"
+            step="0.01"
+            onChange={numberChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            value={enteredDate}
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="expense__actions">
