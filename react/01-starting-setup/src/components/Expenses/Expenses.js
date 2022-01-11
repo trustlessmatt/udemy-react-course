836c11ad9ab7885c401fab81f630d09e761ece20
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-11 15:54:58 -0500

    new ExpensesList component to handle conditional renders

--- react/01-starting-setup/src/components/Expenses/Expenses.js
+++ react/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,41 +1,32 @@
 import { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [selectedYear, setSelectedYear] = useState("2020");
 
-  const dateFilterHandler = chosenYear => {
+  const dateFilterHandler = (chosenYear) => {
     setSelectedYear(chosenYear);
-    console.log("From state: ", selectedYear);
+    console.log("Chosen Year: ", chosenYear);
   };
+
+  // my question was: where does this .items property come from?
+  // answer: it is created as a custom property of <Expenses /> in App.js !
+  // we are passing this prop into our Expenses.js file and using it
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={selectedYear} onDateFilter={dateFilterHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          selected={selectedYear}
+          onDateFilter={dateFilterHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
