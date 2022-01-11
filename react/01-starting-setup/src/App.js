ed7b8104a4dcf871424ccbdfc4c2dfd53524ceb8
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-11 15:54:04 -0500

    made expenses array stateful

--- react/01-starting-setup/src/App.js
+++ react/01-starting-setup/src/App.js
@@ -1,38 +1,48 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+// hard coded expenses array
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  // hard coded expenses array
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);  // initial state is dummy data
 
   const addExpenseHandler = expense => {
-    console.log("In App.js: ", expense);
+    // updating state based on previous state should use function
+    // NOT -> setExpenses([expense, ...expenses]);
+
+    // the correct way:
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+
   };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses}/>
+      <Expenses items={expenses} />
     </div>
   );
 }
